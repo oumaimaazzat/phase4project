0b4268ca238e12b503617ea27348afe8daf23c3f
Jason Herrera <baytownjason@yahoo.com>
2021-03-30 09:20:15 -0500

    nothing new

--- backendMZC/config/routes.rb
+++ backendMZC/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # resources :users
-  # resources :api_v1_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope '/api/v1' do 
 
- 
+    resources :users
+    resources :api_v1_users
     namespace :api do
       namespace :v1 do
         resources :tracks do
