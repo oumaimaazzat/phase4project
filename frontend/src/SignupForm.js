085e097fa21259d9b66cfd8c7b3780a1e06bdb7d
Jason Herrera <baytownjason@yahoo.com>
2021-03-31 14:14:14 -0500

    finally done

---
+++ frontend/src/SignupForm.js
@@ -0,0 +1,24 @@
+//Oumaimas form
+import React, { Component } from 'react'
+import "./loginForm.css"; 
+
+
+
+
+class SignupForm extends Component {
+
+render() {
+    
+    return (
+      <div>
+       
+  <h1>SIGN IN</h1>
+            
+          
+        </div>
+
+        
+        )
+    }
+}
+export default SignupForm
