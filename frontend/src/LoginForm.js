2ea7b3955b2624a7b57584508d3dab9f7f6b3852
Oumaima Azzat <oumaimazzat@gmail.com>
2021-03-31 15:01:20 -0400

    done

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,5 +1,6 @@
 //Oumaimas form
 import React, { Component } from 'react'
+import "./loginForm.css"; 
 
 
 
@@ -9,11 +10,35 @@ class LoginForm extends Component {
 render() {
     
     return (
-        <div className="hi">
-          <h1>LOG IN</h1>
+      <div>
+       
+               <form action="action_page.php" method="post">
+
+
+                          <div class="container">
+                                <label for="uname"><b>Username</b></label>
+                                <input type="text" placeholder="Enter Username" name="uname" required/>
+
+                                    <label for="psw"><b>Password</b></label>
+                                    <input type="password" placeholder="Enter Password" name="psw" required/>
+
+                                    <button type="submit">Login</button>
+                                    <label>
+
+                                    <input type="checkbox" checked="checked" name="remember" /> Remember me
+                                    </label>
+                          </div>
+
+             <div class="container" >
+               <button type="button" class="cancelbtn">Cancel</button>
+                 <span class="psw">Forgot <a href="#">password?</a></span>
+             </div>
+</form>
             
           
         </div>
+
+        
         )
     }
 }
